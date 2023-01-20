27444280142991cf66e192a8eec96682a2138189
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-01-20 12:41:31 -0500

    Adding Navbar component

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -2,11 +2,13 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { HomePage } from './HomePage';
 import { AboutPage } from './AboutPage';
 import { LoginPage } from './LoginPage';
+import { Navbar } from './Navbar';
 
 export const MainApp = () => {
     return (
         <>
             <h1>MainApp</h1>
+            <Navbar />
             <hr />
 
             <Routes>
